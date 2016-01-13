ffbea92d9ec80acf9a69e695e6691dc48774ef36
mr Li <554830231@qq.com>
2016-01-13 12:01:27 +0800

    impletent left and right move

--- js/index.js
+++ js/index.js
@@ -8,34 +8,70 @@ ScrollImage.prototype = {
 		this.nav = this.wrap.getElementsByTagName("ul")[0];
 		this.navList = this.nav.getElementsByTagName("li");
 		this.navListWidth = this.navList[0].offsetWidth;
-		this.wrapWidth();
+		this.leftBtn = this.wrap.getElementsByClassName("left")[0];
+		this.rightBtn = this.wrap.getElementsByClassName("right")[0];
+		//li标签的个数
+		this.liLength = this.navList.length;
+		this.wrapWidth.apply(_that);
+		//每一张图片滚动一个版面所需要的的时间，单位是ms
 		this.durationTime = 1000;
-		this.speed = 0;
-		this.changeSpeed = this.navListWidth/this.durationTime;
+		//每个时段版面的left值，初始为0
+		this.leftPos = 0;
+		//版面滚动的最大left值
+		this.maxLeftPos = (this.liLength-1) * this.navListWidth;
 		this.clearIntervalId = setInterval(function(){
-			_that.inScroll();
-		},1);
+			_that.LeftInScroll();
+		},1500);
+		//定义一个方法用来获取左边按钮点击方法
+		this._leftInScroll = function(){return _that.leftInScroll.apply(_that);};
+		//定义一个方法用来获取右边按钮点击方法
+		this._rightInScroll = function(){return _that.rightInScroll.apply(_that);};
+		//定义一个变量用来判别当前是不是处于运动状态,初始值为0
+		this.distinguishMove = 0;
 		this.wrap.onmouseover = function(){
 			clearInterval(_that.clearIntervalId);
 		};
 		this.wrap.onmouseout = function(){
-			_that.clearIntervalId = setInterval(function(){
-				_that.inScroll();
-			},1);
+			_that.clearIntervalId = setInterval(function(){ 
+				_that.leftInScroll();
+			},1500);
+		};
+		this.addEvent("click",_that.leftBtn,this._leftInScroll);
+		this.addEvent("click",this.rightBtn,this._rightInScroll);
+	},
+	leftInScroll:function(){
+		this.leftPos  += this.navListWidth;
+		var _that = this;
+		if(_that.leftPos <= this.maxLeftPos && _that.leftPos >=0){
+			_that.nav.style.left = -_that.leftPos + 'px';
+		}else{
+			_that.leftPos = 0;
+			_that.nav.style.left = 0+ 'px';
 		};
 	},
-	inScroll:function(){
+	rightInScroll:function(){
 		var _that = this;
-		if(this.speed<=(this.navListWidth * (this.navList.length-1))){
-			this.nav.style.left = -this.speed + 'px';
+		_that.leftPos -= _that.navListWidth;
+		if(_that.leftPos<= this.maxLeftPos && _that.leftPos >=0){
+			_that.nav.style.left = -_that.leftPos + 'px';
 		}else{
-			this.speed = 0;
-			this.nav.style.left = 0+ 'px';
+			_that.leftPos = _that.maxLeftPos;
+			_that.nav.style.left = -_that.maxLeftPos + 'px';
 		};
-		this.speed  += this.changeSpeed;
 	},
 	wrapWidth:function(){
 		var _that = this;
 		this.nav.style.width = (this.navListWidth * this.navList.length) + 'px';
+	},
+	addEvent:function(type,dom,handler){
+		return dom.addEventListener?dom.addEventListener(type,handler,false):dom.attachEvent("on"+type,handler);
+	},
+	moveFun:function(distance){
+		console.log("moveFun");
+		var _that = this;
+		//定义一个变量用来存储单位时间需要走动的距离
+		var changePos = distance/this.durationTime;
+		this.nav.style.left = changePos + 'px';
+		var moveIntervalId = setInterval(arguments.callee,1);
 	}
 };
